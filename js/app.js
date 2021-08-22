7ea507cda02c50be38dbb6ee1c406d6c61da8105
MD SAKIBUL ALAM UTCHAS <63851422+msa-utchas@users.noreply.github.com>
2021-08-22 22:00:16 +0600

    necessary comments added

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 /*jshint esversion: 6 */
-let isPromoApplied = false;
+let isPromoApplied = false; //promo code flag
+
+//memory configuration 
 document.getElementById("ram-option-1").addEventListener('click', function () {
     document.getElementById("extra-memory-cost").innerText = '0';
     totalCost();
@@ -8,6 +10,7 @@ document.getElementById("ram-option-2").addEventListener('click', function () {
     document.getElementById("extra-memory-cost").innerText = '180';
     totalCost();
 });
+//storage configuration
 document.getElementById("storage-option-1").addEventListener('click', function () {
     document.getElementById("extra-storage-cost").innerText = '0';
     totalCost();
@@ -20,6 +23,7 @@ document.getElementById("storage-option-3").addEventListener('click', function (
     document.getElementById("extra-storage-cost").innerText = '180';
     totalCost();
 });
+//delevery date selection
 document.getElementById("delivery-option-1").addEventListener('click', function () {
     document.getElementById("extra-delivery-cost").innerText = '0';
     totalCost();
@@ -28,6 +32,8 @@ document.getElementById("delivery-option-2").addEventListener('click', function 
     document.getElementById("extra-delivery-cost").innerText = '20';
     totalCost();
 });
+
+//promo code action
 document.getElementById("promo-code-apply-btn").addEventListener('click', function () {
     const promoCode = document.getElementById("input-promo-code").value;
     console.log(promoCode);
@@ -36,6 +42,8 @@ document.getElementById("promo-code-apply-btn").addEventListener('click', functi
     document.getElementById("input-promo-code").value = '';
 
 });
+
+//total cost calculation function
 function totalCost() {
     const bestPrice = parseFloat(document.getElementById("best-price").innerText);
     const extraMemoryCost = parseFloat(document.getElementById("extra-memory-cost").innerText);
@@ -45,6 +53,8 @@ function totalCost() {
     document.getElementById("final-price").innerText = (bestPrice + extraMemoryCost + ExtraStorageCost + extraDeliveryCHarge).toString();
     isPromoApplied = false;
 }
+
+//promo code application function
 function applyPromoCode(code) {
     if (code === 'stevekaku' && isPromoApplied===false)
     {
